fe3a474c7c5e9e7bdb6a61f3d3b521d05e78b858
Erwin Méndez <erwinmendez0911@hotmail.com>
2022-07-22 11:33:55 -0400

    Adding fade animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,8 @@ module.exports = {
     extend: {
       animation: {
         matched: "matched 0.8s linear normal both",
+        "fade-in": "fade 0.7s linear normal both",
+        "fade-out": "fade 0.3s linear reverse both",
       },
       keyframes: {
         matched: {
@@ -14,6 +16,13 @@ module.exports = {
           "60%": { padding: "0.5rem" },
           "80%": { padding: "0.25rem" },
           "100%": { padding: "0em", backgroundColor: "rgb(248 250 252)" },
+        },
+        fade: {
+          "0%": { opacity: 0, transform: "translateY(-100px)" },
+          "25%": { opacity: 0.25, transform: "translateY(-75px)" },
+          "50%": { opacity: 0.5, transform: "translateY(-50px)" },
+          "75%": { opacity: 0.75, transform: "translateY(-25px)" },
+          "100%": { opacity: 1, transform: "translateY(0px)" },
         },
       },
     },
